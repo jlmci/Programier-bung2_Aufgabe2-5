93cae4d330e04a007c523dc9c7742d97a5dfd63d
jlmci <lj9092@mci4me.at>
2025-05-22 16:06:19 +0200

    alls hiiin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from PIL import Image #paket für Bilder
+
 import read_data
+
 
 st.write("# EKG APP")
 st.write("## Versuchsperson auswählen")
@@ -13,4 +16,21 @@ st.session_state.current_user = st.selectbox(
     'Versuchsperson',
     options = read_data.create_name_list(), key="sbVersuchsperson")
 
+
+#person_data = read_data.load_person_data()
+#data_of_person = read_data.get_person_data_ba_name(person_data, st.session_state.current_user)
+
+person_names = read_data.create_name_list
+if st.session_state.current_user in person_names:
+    st.session_state.picture_path = read_data.find_person_data_by_name(st.session_state.current_user)["picture_path"]
+
+# ...
+
+# Öffne das Bild und Zeige es an
+image = Image.open("../" + st.session_state.picture_path)
+st.image(image, caption=st.session_state.current_user)
+
 st.write("Der Name ist: ", st.session_state.current_user) 
+
+#image = Image.open(data_of_person["picture_path"])
+#st.image(image, caption=st.session_state.current_user)
--- read_data.py
+++ read_data.py
@@ -16,6 +16,16 @@ def create_name_list():
     namen_liste = [f"{person['firstname']} {person['lastname']}" for person in person_data]
     return namen_liste
 
+def get_person_data_ba_name(person_data, name_to_find):
+    name_to_find = name_to_find.strip()
+    name_to_find.split(", ")
+    
+    for i in person_data:
+        # Check if the name_to_find is in the firstname or lastname of the person
+        if name_to_find[0] in i["firstname"] and name_to_find[1] in i["lastname"]:
+            return i
+
+    return None
 
 
 
@@ -27,3 +37,4 @@ if __name__ == "__main__":
     print("Person data loaded successfully.")
     print(person_data)
     print(person_name_list)
+    print("Picture path: ", picture_path(person_data))
