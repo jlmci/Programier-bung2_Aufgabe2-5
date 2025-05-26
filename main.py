99a3a4decf926c7b89cee3b8cbee494e1e36e3fd
J4nickH <ha1220@mci4me.at>
2025-05-26 17:50:38 +0200

    mockup nachgebaut

--- main.py
+++ main.py
@@ -2,50 +2,55 @@ import streamlit as st
 from PIL import Image #paket für Bilder
 
 import read_data
-
+# Importiere die Funktionen aus read_data.py
+# Lade die Personendaten und die Namensliste
 data = read_data.load_person_data()
+person_names = read_data.create_name_list()
+
+# App Titel
 st.write("# EKG APP")
-st.write("## Versuchsperson auswählen")
 
 # Session State wird leer angelegt, solange er noch nicht existiert
 if 'current_user' not in st.session_state:
     st.session_state.current_user = 'None'
-    print(st.session_state.current_user)
-
-# Dieses Mal speichern wir die Auswahl als Session State
-st.session_state.current_user = st.selectbox(
-    'Versuchsperson',
-    options = read_data.create_name_list(), key="sbVersuchsperson")
 
 
-#person_data = read_data.load_person_data()
-#data_of_person = read_data.get_person_data_ba_name(person_data, st.session_state.current_user)
+#Oberste Zeile des programms
+ueberschrift, person_auswahl = st.columns([1,2], gap="small")
+with ueberschrift:
+    st.markdown("<div style='padding-top: 23px; font-size: 32px;'>Dashboard von</div>", unsafe_allow_html=True)
+with person_auswahl:
+    st.session_state.current_user = st.selectbox('', options = read_data.create_name_list())
 
-person_names = read_data.create_name_list()
+#st.markdown("<br>", unsafe_allow_html=True)  # Leerer Platzhalter, um den Abstand zu vergrößern
+
+
 try:
     st.session_state.picture_path = read_data.get_person_data_ba_name(data, st.session_state.current_user)["picture_path"]
     if st.session_state.current_user in person_names:
         st.session_state.picture_path = read_data.get_person_data_ba_name(data, st.session_state.current_user)["picture_path"]
 except:
     st.session_state.picture_path = "data/pictures/none.jpg"
-
-
-# Öffne das Bild und Zeige es an
 image = Image.open(st.session_state.picture_path)
-st.image(image, caption=st.session_state.current_user)
 
-st.write("Der Name ist: ", st.session_state.current_user) 
 
-col1, col2 = st.columns(2)
 
-with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
+id_value = read_data.get_person_data_ba_name(data, st.session_state.current_user)["id"]
+birthdate_value = read_data.get_person_data_ba_name(data, st.session_state.current_user)["date_of_birth"]
+# Bild und Personendaten nebeneinander anzeigen
+bild, personendaten = st.columns([1,2], gap="small")
+with bild:
+    st.header("Profilbild")
+    st.image(image)
+with personendaten:
+    st.header("Personendaten")
+    st.write("Vorname: ", read_data.get_person_data_ba_name(data, st.session_state.current_user)["firstname"])
+    st.write("Nachname: ", read_data.get_person_data_ba_name(data, st.session_state.current_user)["lastname"])
+    st.markdown(f"<span style='color:white; font-size:16px;'>id: {id_value}</span>", unsafe_allow_html=True)
+    st.markdown(f"<span style='color:white; font-size:16px;'>id: {birthdate_value}</span>", unsafe_allow_html=True)
+    st.write("Geschlecht: ", read_data.get_person_data_ba_name(data, st.session_state.current_user)["gender"])
 
-with col2:
-    st.header("A dog")
-    st.image("https://static.streamlit.io/examples/dog.jpg")
 
-with col3:
-    st.header("An owl")
-    st.image("https://static.streamlit.io/examples/owl.jpg")
+st.write("## EKG-Daten")
+# Hier können Sie die EKG-Daten anzeigen, die zu der ausgewählten Person gehören
+
